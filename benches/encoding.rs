e0943d454a1858b400c1650975f7d23a80cb6804
Gabriel Féron <gabriel@phnx.im>
2026-03-31 15:59:49 +0200

    Add missing license headers

--- benches/encoding.rs
+++ benches/encoding.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use mimi_content::{
     cbor,
     content_container::{Disposition, ExtensionName, MimiContent, NestedPart, PartSemantics},
--- minicbor-custom-enum/src/lib.rs
+++ minicbor-custom-enum/src/lib.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
